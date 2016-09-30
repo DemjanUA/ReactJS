08ac5f1c7d0661f5397640aadbc3ecbc445c3f6e
Demjan <Demjan>
2016-09-30 15:12:02 +0300

    'added

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-"use strict";
+'use strict';
 
+var storge = [{
+    'imageURL': 'https://facebook.github.io/react/img/logo.svg',
+    'title': 'React',
+    'subtitle': 'Библиотека для создания пользовательских интерфейсов'
+}, {
+    'imageURL': 'https://angular.io/resources/images/logos/angular2/angular.svg',
+    'title': 'Angular 2',
+    'subtitle': 'Один фреймворк'
+}, {
+    'imageURL': 'https://worldvectorlogo.com/logos/codeigniter.svg',
+    'title': 'CodeIgniter',
+    'subtitle': 'Powerful PHP framework'
+}, {
+    'imageURL': 'https://worldvectorlogo.com/logos/laravel.svg',
+    'title': 'Laravel',
+    'subtitle': 'The PHP Framework For Web Artisans'
+}, {
+    'imageURL': 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Boostrap_logo.svg',
+    'title': 'Bootstrap 4',
+    'subtitle': 'HTML, CSS, and JS framework'
+}, {
+    'imageURL': 'http://sass-scss.ru/assets/img/logos/logo.svg',
+    'title': '	Sass',
+    'subtitle': 'CSS extension language'
+}];
+//console.log(storge);
 var Hero = React.createClass({
-    displayName: "Hero",
+    displayName: 'Hero',
 
     getInitialState: function getInitialState() {
         return {
@@ -15,21 +41,21 @@ var Hero = React.createClass({
 
     render: function render() {
         return React.createElement(
-            "div",
-            { className: "container" },
+            'div',
+            { className: 'container' },
             React.createElement(
-                "div",
-                { className: "count" },
+                'div',
+                { className: 'count' },
                 this.state.count
             ),
-            React.createElement("img", { src: this.props.imageURL, onClick: this.handleClick }),
+            React.createElement('img', { src: this.props.imageURL, onClick: this.handleClick }),
             React.createElement(
-                "h1",
+                'h1',
                 null,
                 this.props.title
             ),
             React.createElement(
-                "p",
+                'p',
                 null,
                 this.props.subtitle
             )
@@ -37,17 +63,22 @@ var Hero = React.createClass({
     }
 });
 
-ReactDOM.render(React.createElement(
-    "div",
-    null,
-    React.createElement(Hero, {
-        imageURL: "https://facebook.github.io/react/img/logo.svg",
-        title: "React",
-        subtitle: "Библиотека для создания пользовательских интерфейсов"
-    }),
-    React.createElement(Hero, {
-        imageURL: "https://angular.io/resources/images/logos/angular2/angular.svg",
-        title: "Angular 2",
-        subtitle: "Один фреймворк"
-    })
-), document.getElementById('root'));
+var HeroList = React.createClass({
+    displayName: 'HeroList',
+
+    render: function render() {
+        return React.createElement(
+            'div',
+            { className: 'clearfix' },
+            storge.map(function (element) {
+                return React.createElement(Hero, {
+                    imageURL: element.imageURL,
+                    title: element.title,
+                    subtitle: element.subtitle
+                });
+            })
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(HeroList, null), document.getElementById('root'));
--- app.jsx
+++ app.jsx
@@ -1,30 +1,36 @@
-const storge = [
+const storage = [
     {
+        'id' : 1,
         'imageURL' : 'https://facebook.github.io/react/img/logo.svg',
         'title' : 'React',
         'subtitle' : 'Библиотека для создания пользовательских интерфейсов'
      },
     {
+        'id' : 2,
         'imageURL' : 'https://angular.io/resources/images/logos/angular2/angular.svg',
         'title' : 'Angular 2',
         'subtitle' : 'Один фреймворк'
      },
     {
+        'id' : 3,
         'imageURL' : 'https://worldvectorlogo.com/logos/codeigniter.svg',
         'title' : 'CodeIgniter',
         'subtitle' : 'Powerful PHP framework'
      },
     {
+        'id' : 4,
         'imageURL' : 'https://worldvectorlogo.com/logos/laravel.svg',
         'title' : 'Laravel',
         'subtitle' : 'The PHP Framework For Web Artisans'
      },
     {
+        'id' : 5,
         'imageURL' : 'https://upload.wikimedia.org/wikipedia/commons/e/ea/Boostrap_logo.svg',
         'title' : 'Bootstrap 4',
         'subtitle' : 'HTML, CSS, and JS framework'
      },
     {
+        'id' : 6,
         'imageURL' : 'http://sass-scss.ru/assets/img/logos/logo.svg',
         'title' : '	Sass',
         'subtitle' : 'CSS extension language'
@@ -55,12 +61,31 @@ var Hero = React.createClass({
 });
 
 var HeroList = React.createClass({
+    getInitialState : function() {
+        return {
+            list : storage
+        };
+    },
+
+    componentDidMount : function() {
+        console.log(this.state.list);
+    },
+
+    handleSearch : function(event) {
+        console.log(event.target.value);
+    },
+
     render : function() {
         return (
             <div className="clearfix">
+                <p className="search">
+                    <label>
+                        <input type="text" onChange={ this.handleSearch } />
+                    </label>
+                </p>
                 {
-                    storge.map(function(element) {
-                        return (<Hero 
+                    storage.map(function(element) {
+                        return (<Hero key={ element.id }
                             imageURL={ element.imageURL }
                             title={ element.title }
                             subtitle={ element.subtitle }
